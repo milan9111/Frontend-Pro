92d0996ae5e6f7da145c33447167bfafa99f45fa
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-01-30 15:03:44 +0200

    homework 2.3

--- js/custom.js
+++ js/custom.js
@@ -9,7 +9,7 @@ if (userAge == false && userSmoke == false) {
     alert('Молодец, не надо');
 }  else if (userAge == true && userSmoke == true) {
     alert('Ну и зря!');
-}*/
+}
 
 
 var firstValue = prompt('Введите первое значение');
@@ -21,4 +21,20 @@ if (firstValue > secondValue) {
     alert(secondValue + ' больше, чем ' + firstValue);
 } else {
    alert('Значения равны');
-} 
+}*/
+
+var kmDistance = prompt('Введите расстояние в километрах');
+var ftDistance = prompt('Введите расстоянине в футах');
+var smDistance = (kmDistance * 100000);
+var newValueFt = (ftDistance * 30.5);
+
+if (smDistance > newValueFt) {
+    alert(kmDistance + ' км больше, чем ' + ftDistance + ' футов');
+}   else if (newValueFt > smDistance) {
+    alert(ftDistance + ' футов больше, чем ' + kmDistance + ' км');
+}   else if (newValueFt === smDistance) {
+    alert('Расстояние равно');
+}
+
+
+
