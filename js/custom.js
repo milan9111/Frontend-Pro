77d596fac045d2fb0623c4cafa29b8dac77e6a43
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-02-19 23:56:54 +0200

    homework  07

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,27 @@
+var numbers = [-17, -16, -2, 4, 9, 25, 33, 54, 71, 256]; 
+function positive(item, index, array){
+    return item > 0
+};
 
+var positiveNumbers = numbers.filter(positive);
 
+var squareRootNumbers = positiveNumbers.map(function(item, index, array) {
+	return Math.sqrt(item);
+});
 
+console.log(squareRootNumbers);
+
+//---------------------------------------------------------------------------
+
+var arr = [1, 4, 2, 2, 1, 2, 6];
+var number = 0;
+var result = arr.reduce(function(previousValue, item, index, array) {
+	if (previousValue > 10) {
+		console.log(number);
+		return;
+	} else {
+		number++;
+		return previousValue + item;
+	}
+}); 
+ 
