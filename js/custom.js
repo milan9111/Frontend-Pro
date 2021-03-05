2814cc7346a24f74a9adb6a449091e60271fba52
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-03-05 23:12:27 +0200

    homework 11

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,22 @@
 
+var elem = document.getElementById('elem');
 
+function func(surname, name) {
+alert(this.value + ', ' + surname + ' ' + name);
+}
+var funcName = func.bind(elem);
 
+funcName('Иванов', 'Иван');
+funcName('Петров', 'Петр');
+
+//-------------------------------------------------
+
+var elem = document.getElementById('elem');
+
+function funcHI(text) {
+alert(this.value + text);
+}
+
+funcHI.call(elem, ' <--- value инпута');
+
+//-------------------------------------------------
