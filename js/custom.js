8b7b944389758444a90c7412a0ccd15a5936e54a
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-03-26 22:33:24 +0200

    homework 16 new

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,4 @@
-let blocks = document.querySelectorAll('.pane');
-for (let i = 0; i < blocks.length; i++) {
-    blocks[i].insertAdjacentHTML("afterbegin", '<button class="close"><img src="./images/delete.gif" alt="x"></button>');
-    blocks[i].firstChild.onclick = function() {
-        blocks[i].remove();
-    }
-  };
+
 
 
 
