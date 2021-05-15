d082ad866b6ebdedcd50ae71de9e5f069b458804
vasiliy_zakrhgevskyi <milan9111@ukr.net>
2021-05-15 17:07:35 +0300

    homework 29

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,43 @@
 let inputList = document.querySelectorAll('input');
 
-for (let i = 0; i < inputList.length; i++) {
-    inputList[i].addEventListener('input', function() {
-        if (this.value = this.value.replace(/[^\d]/g, '')) {
-          this.nextElementSibling.focus();  
-        } 
-    });
-    inputList[i].addEventListener('keyup', function(event) {
-        if (event.key === "Backspace") {
-            this.previousElementSibling.focus();
-        }
-    });
+
+function setNumbers(nodeList) {
+    const numbersArray = getCookies(nodeList.length);
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].value = numbersArray[i]
+        nodeList[i].addEventListener('input', function() {
+            if (this.value = this.value.replace(/[^\d]/g, '')) { 
+                setCookie(i,this.value, nodeList.length);
+                this.nextElementSibling.focus();  
+            } 
+        });
+    }    
 }
-    
+
+function delNumbers(nodeList) {
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].addEventListener('keyup', function(event) {
+            if (event.key === "Backspace") {
+                setCookie(i, event.target.value, nodeList.length);
+                this.previousElementSibling.focus();
+            }
+        });
+    }
+}
+
+function setCookie(position, number, length){
+    let numbers = getCookies(length);
+    numbers[position] = number;
+    document.cookie = `cart=${numbers.join(',')}`;
+}
+
+function getCookies(length){
+    let numbersArray = document.cookie?.split('=')[1]?.split(',') || Array(length).fill(null);
+    return numbersArray;		
+}
+setNumbers(inputList);
+delNumbers(inputList);
+
+
+
+
